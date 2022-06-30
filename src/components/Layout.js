b85695552c2b31f96ec9d737643f21fa271665fb
harveybland <harvey.bland@genius.online>
2022-06-30 13:34:16 +0100

    Initial commit

---
+++ src/components/Layout.js
@@ -0,0 +1,15 @@
+import React from "react"
+import Navbar from "./Navbar"
+import "../styles/"
+
+export default function Layout({ children }) {
+  return (
+    <div className="layout">
+      <Navbar />
+      <div className="content">{children}</div>
+      <footer>
+        <p>Copyright 2021</p>
+      </footer>
+    </div>
+  )
+}
